606e259b1753bbf9d30872bc78e44f7780c15d9b
U-HOME\Sarath <sarath.peruri@gmail.com>
2024-09-16 18:09:06 +0530

    pascal triangle explnantion prog push

--- GFG.java
+++ GFG.java
@@ -1,33 +1,67 @@
+// Java Program to Print Pascal's Triangle
+
+// Importing input output classes
 import java.io.*;
 
-class GFG {
-    public int factorial(int i)
+// Main Class
+public class GFG {
+
+    // Method 1
+    // To find factorial of a number
+    public int factorial(int a)
     {
-        if (i == 0)
+        // Edge case
+        // Factorial of 0 is unity
+        if (a == 0)
+
+            // Hence return 1
             return 1;
-        return i * factorial(i - 1);
+
+        // else recursively call the function over the
+        // number whose factorial is to be computed
+        return a * factorial(a - 1);
     }
+
+    // Method 2
+    // Main driver method
     public static void main(String[] args)
     {
-        int n = 4, i, j;
-        GFG g = new GFG();
-        for (i = 0; i <= n; i++) {
-            for (j = 0; j <= n - i; j++) {
+        // Declare and initialize number whose
+        // factorial is to be computed
+        int k = 4;
 
-                // for left spacing
+        int a, b;
+
+        // Creating an object of GFG class
+        // in the main() method
+        GFG g = new GFG();
+
+        // iterating using nested for loop to traverse over
+        // matrix
+
+        // Outer for loop
+        for (a = 0; a <= k; a++) {
+
+            // Inner loop 1
+            for (b = 0; b <= k - a; b++) {
+
+                // Print white space for left spacing
                 System.out.print(" ");
             }
-            for (j = 0; j <= i; j++) {
+
+            // Inner loop 2
+            for (b = 0; b <= a; b++) {
 
                 // nCr formula
                 System.out.print(
                     " "
-                    + g.factorial(i)
-                          / (g.factorial(i - j)
-                             * g.factorial(j)));
+                    + g.factorial(a)
+                          / (g.factorial(a - b)
+                             * g.factorial(b)));
             }
 
-            // for newline
+            // By now, we are done with one row so
+            // a new line
             System.out.println();
         }
     }
