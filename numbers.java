ec5f52d5c5e7153ade90badda6594a1a0949dfbe
U-HOME\Sarath <sarath.peruri@gmail.com>
2024-09-16 16:04:33 +0530

    third

---
+++ numbers.java
@@ -0,0 +1,35 @@
+public class numbers{
+	public static void main(String[] args) {
+
+//The byte data type can store whole numbers from -128 to 127.
+
+byte mybyte = 100;
+System.out.println(mybyte);
+
+//The short data type can store whole numbers from -32768 to 32767.
+
+short myshort = 5000;
+System.out.println(myshort);
+
+//The int data type can store whole numbers from -2147483648 to 2147483647.
+
+int myint = 100000;
+System.out.println(myint);
+
+//The long data type can store whole numbers from -9223372036854775808 to 9223372036854775807.
+
+long mylong = 15000000000L;
+System.out.println(mylong);
+
+//You should use a floating point type whenever you need a number with a decimal, such as 9.99 or 3.14515.
+
+//The float and double data types can store fractional numbers. Note that you should end the value with an "f" for floats and "d" for doubles
+
+float myfloat = 5.75f;
+System.out.println(myfloat);
+
+double mydouble = 19.99d;
+System.out.println(mydouble);
+
+}
+}
