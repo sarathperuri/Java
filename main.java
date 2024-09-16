4dd8b1b18e10c0d2b917058dc89186378362571d
sarathperuri <sarath.peruri@gmail.com>
2024-09-16 14:45:11 +0530

    adding project to git hib

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("I am learning Java.");
+    System.out.println("It is awesome!");
+    //System.out.println(This sentence will produce an error);
+    System.out.println(3);
+    System.out.println(358);
+    System.out.println(50000);
+    System.out.println(3 + 3);
+    System.out.println(2 * 5);
+  }
+}
---
+++ variables.java
@@ -0,0 +1,37 @@
+public class variables {
+	public static void main(String[] args) {
+		String name = "John";
+		System.out.println(name);
+		
+		int myNum = 15;
+		System.out.println(myNum);
+
+		int mynUm;
+		mynUm = 14;
+		System.out.println(mynUm);
+
+		int mynuM = 15;
+		mynuM = 20;  // myNum is now 20
+		System.out.println(mynuM);
+
+		int mYnum = 5;
+		float myFloatNum = 5.99f;
+		char myLetter = 'D';
+		boolean myBool = true;
+		String myText = "Hello";
+		System.out.println(mYnum);
+		System.out.println(myFloatNum);
+		System.out.println(myLetter);
+		System.out.println(myBool);
+		System.out.println(myText);
+
+		String firstName = "John ";
+		String lastName = "Doe";
+		String fullName = firstName + lastName;
+		System.out.println(fullName);
+
+		int x = 5;
+		int y = 6;
+		System.out.println(x + y); // Print the value of x + y
+	}
+}
